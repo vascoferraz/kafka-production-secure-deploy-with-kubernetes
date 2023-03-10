dcd79022f5ef45fdc489573db1efff92e8cdf472
Vasco Ferraz <vascoferraz@hotmail.com>
2023-03-10 21:33:33 +0000

    Update uninstall script

--- scripts/8-uninstall.sh
+++ scripts/8-uninstall.sh
@@ -3,15 +3,19 @@
 # Set the current tutorial directory
 export TUTORIAL_HOME="./.."
 
+# Set context to confluent
+kubectl config set-context --current --namespace=confluent
+
 for rb in $(kubectl -n confluent get cfrb --no-headers -ojsonpath='{.items[*].metadata.name}'); do kubectl -n confluent  patch cfrb $rb -p '{"metadata":{"finalizers":[]}}' --type=merge; done
 
-kubectl delete confluentrolebinding --all --namespace confluent
-kubectl delete -f $TUTORIAL_HOME/manifests/confluent-platform-production.yaml --namespace confluent
-kubectl delete secret ksqldb-mds-client sr-mds-client connect-mds-client krp-mds-client c3-mds-client mds-client --namespace confluent
-kubectl delete secret mds-token --namespace confluent
-kubectl delete secret credential --namespace confluent
-kubectl delete secret tls-group1 --namespace confluent
-helm delete test-ldap --namespace confluent
-helm delete operator --namespace confluent
+kubectl delete confluentrolebinding --all
+kubectl delete -f $TUTORIAL_HOME/manifests/confluent-platform-production.yaml
+kubectl delete secret ksqldb-mds-client sr-mds-client connect-mds-client krp-mds-client c3-mds-client mds-client
+kubectl delete secret mds-token
+kubectl delete secret credential
+kubectl delete secret tls-group1
+kubectl delete pod alpine
+helm delete test-ldap
+helm delete operator
 helm delete kafka-ui
 helm delete postgresql
