26b60161ac27ce09cbffa1c2c020d093d5c87c64
Vasco Ferraz <vascoferraz@hotmail.com>
2023-03-25 15:40:21 +0000

    Fix key and value schemas

--- usecases/syslog/deploy-syslog.sh
+++ usecases/syslog/deploy-syslog.sh
@@ -5,11 +5,11 @@ export TUTORIAL_HOME="./../../"
 export SOURCE_CONNECTOR="syslog-source-connector"
 
 # Build custom Alpine image
-docker build -t alpine-vf:3.17.2 $TUTORIAL_HOME/docker-images/alpine
+docker build -t alpine-vf:3.17.2 $TUTORIAL_HOME/docker-images/alpine-syslog
  
 # Deploy alpine container for the syslog generator
-kubectl apply -f $TUTORIAL_HOME/manifests/alpine.yaml
-kubectl wait --for=condition=Ready pod/alpine --timeout=60s
+kubectl apply -f $TUTORIAL_HOME/manifests/alpine-syslog.yaml
+kubectl wait --for=condition=Ready pod/alpine-syslog --timeout=60s
 
 # Create syslog topic
 kubectl exec -it kafka-0 -c kafka -- kafka-topics --create --bootstrap-server kafka.confluent.svc.cluster.local:9092 --command-config /opt/confluentinc/etc/kafka/kafka.properties --topic syslog --replication-factor 3 --partitions 3
@@ -37,4 +37,4 @@ kubectl cp ./connectors/syslog-source-connector.json confluent/connect-0:/tmp/ -
 kubectl exec -it connect-0 -c connect -- curl -s -k -X PUT -H 'Content-Type:application/json' --data @/tmp/syslog-source-connector.json https://connect-0.connect.confluent.svc.cluster.local:8083/connectors/$SOURCE_CONNECTOR/config -u testadmin:testadmin
 
 # Start the syslog generator python script on the alpine container
-kubectl exec -t alpine -c alpine -- nohup /usr/sbin/syslog_gen.py --host connect-0.connect.confluent.svc.cluster.local --port 5559 --file /usr/sbin/dataset.txt --count 1 --sleep 1 1> /dev/null 2>&1 &
+kubectl exec -t alpine-syslog -c alpine-syslog -- nohup /usr/sbin/syslog_gen.py --host connect-0.connect.confluent.svc.cluster.local --port 5559 --file /usr/sbin/dataset.txt --count 1 --sleep 1 1> /dev/null 2>&1 &
