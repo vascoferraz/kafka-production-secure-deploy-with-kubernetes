404629167d0bbc1975594d439103aa20c79d3c08
Vasco Ferraz <vascoferraz@hotmail.com>
2023-04-19 09:34:41 +0100

    Fix TLS for the sink connector in syslog usecase (#29)

--- usecases/syslog/deploy-syslog.sh
+++ usecases/syslog/deploy-syslog.sh
@@ -12,6 +12,14 @@ docker build -t alpine-vf:3.17.3 $TUTORIAL_HOME/docker-images/alpine-syslog
 kubectl apply -f $TUTORIAL_HOME/manifests/alpine-syslog.yaml
 kubectl wait --for=condition=Ready pod/alpine-syslog --timeout=60s
 
+# Copy CA and PostgreSQL certificate and private key into the Kafka Connect pod
+kubectl cp $TUTORIAL_HOME/assets/certs/generated/postgres.pem confluent/connect-0:/tmp/ -c connect
+kubectl cp $TUTORIAL_HOME/assets/certs/generated/postgres-key.pem confluent/connect-0:/tmp/ -c connect
+kubectl cp $TUTORIAL_HOME/assets/certs/generated/ca.pem confluent/connect-0:/tmp/ -c connect
+
+# Convert the PostgreSQL private key from PEM to DER
+kubectl exec -it connect-0 -c connect -- openssl pkcs8 -topk8 -v1 PBE-SHA1-3DES -inform PEM -outform DER -in /tmp/postgres-key.pem -out /tmp/postgresql.pk8 -passout pass:password
+
 # Create syslog topic
 kubectl exec -it kafka-0 -c kafka -- kafka-topics --create --bootstrap-server kafka.confluent.svc.cluster.local:9092 --command-config /opt/confluentinc/etc/kafka/kafka.properties --topic syslog --replication-factor 3 --partitions 3
 
@@ -24,11 +32,11 @@ kubectl exec -it schemaregistry-0 -c schemaregistry -- bash -c 'echo -n $(tr -d 
 kubectl exec -it schemaregistry-0 -c schemaregistry -- curl -k -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data @/tmp/value https://localhost:8081/subjects/syslog-value/versions --user sr:sr-secret
 kubectl exec -it schemaregistry-0 -c schemaregistry -- rm /tmp/syslog-value.avsc /tmp/value
 
-# Copy and deploy source connector config into the pod
+# Copy and deploy source connector config into the Kafka Connect pod
 kubectl cp ./connectors/syslog-source-connector.json confluent/connect-0:/tmp/ -c connect
 kubectl exec -it connect-0 -c connect -- curl -s -k -X PUT -H 'Content-Type:application/json' --data @/tmp/syslog-source-connector.json https://connect-0.connect.confluent.svc.cluster.local:8083/connectors/$SOURCE_CONNECTOR/config -u testadmin:testadmin
 
-# Copy and deploy sink connector config into the pod
+# Copy and deploy sink connector config into the Kafka Connect pod
 kubectl cp ./connectors/syslog-sink-connector.json confluent/connect-0:/tmp/ -c connect
 kubectl exec -it connect-0 -c connect -- curl -s -k -X PUT -H 'Content-Type:application/json' --data @/tmp/syslog-sink-connector.json https://connect-0.connect.confluent.svc.cluster.local:8083/connectors/$SINK_CONNECTOR/config -u testadmin:testadmin
 
