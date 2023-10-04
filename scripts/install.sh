7d96fb5351ba1d4ef36f97657f1516c577ab3512
Eugene Butan <eugene.butan@gmail.com>
2023-10-04 14:06:59 +0100

    Further refactoring

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-set -e  # Exit on errors
+set -xe  # Echo and exit on errors
 
 BIN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 # Set the current tutorial directory
-TUTORIAL_HOME="${BIN_DIR}/.."
+TUTORIAL_HOME="$(realpath "${BIN_DIR}/..")"
 
 CERT_SRC_DIR="${TUTORIAL_HOME}/assets/certificates/sources"
 CERT_OUT_DIR="${TUTORIAL_HOME}/assets/certificates/generated"
@@ -18,16 +17,13 @@ CA_CONFIG_PATH="${CERT_SRC_DIR}/ca-config.json"
 
 # Install dependencies on Mac OS
 brew install cfssl mysql java
-
 # echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
 PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 
 # Build custom Kafka Broker image
 docker build -t confluentinc/cp-server-vf:7.5.0 "${DOCKER_IMAGE_DIR}/kafka"
-
 # Build custom Kafka Connect image
 docker build -t confluentinc/cp-server-connect-vf:7.5.0 "${DOCKER_IMAGE_DIR}/connect"
-
 # Build custom Schema Registry image
 docker build -t confluentinc/cp-schema-registry-vf:7.5.0 "${DOCKER_IMAGE_DIR}/schema-registry"
 
@@ -38,10 +34,12 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add cetic https://cetic.github.io/helm-charts
 helm repo update
 
-# Deploy Confluent for Kubernetes
+# Create namespace and set context to it 
 kubectl create namespace confluent
 kubectl config set-context --current --namespace=confluent
-helm upgrade --install operator confluentinc/confluent-for-kubernetes --namespace confluent
+
+# Deploy Confluent for Kubernetes
+helm upgrade --install --version 0.824.2 operator confluentinc/confluent-for-kubernetes
 CONFLUENT_OPERATOR_POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep confluent-operator)
 kubectl wait --for=condition=Ready pod/${CONFLUENT_OPERATOR_POD_NAME} --timeout=60s
 
@@ -51,68 +49,15 @@ mkdir "${CERT_OUT_DIR}" && cfssl gencert -initca "${CERT_SRC_DIR}/ca-csr.json" |
 # Validate Certificate Authority
 openssl x509 -in "${CA_CERT_PATH}" -text -noout
 
-# Create server certificates with the appropriate SANs (SANs listed in server-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/server-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/server"
-
-# Validate server certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/server.pem" -text -noout
-
-# Create ldap certificates with the appropriate SANs (SANs listed in ldap-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/ldap-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/ldap"
-
-# Validate ldap certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/ldap.pem" -text -noout
-
-# Create Kafka-UI certificates with the appropriate SANs (SANs listed in kafka-ui-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/kafka-ui-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/kafka-ui"
-
-# Validate Kafka-UI certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/kafka-ui.pem" -text -noout
-
-# Create phpldapadmin certificates with the appropriate SANs (SANs listed in phpldapadmin-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/phpldapadmin-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/phpldapadmin"
-
-# Validate phpldapadmin certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/phpldapadmin.pem" -text -noout
-
-# Create postgres certificates with the appropriate SANs (SANs listed in postgres-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/postgres-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/postgres"
-
-# Validate postgres certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/postgres.pem" -text -noout
-
-# Create mysql certificates with the appropriate SANs (SANs listed in mysql-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/mysql-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/mysql"
-
-# Validate mysql certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/mysql.pem" -text -noout
-
-# Create mariadb certificates with the appropriate SANs (SANs listed in mariadb-domain.json)
-cfssl gencert -ca="${CA_CERT_PATH}" \
--ca-key="${CA_KEY_PATH}" \
--config="${CA_CONFIG_PATH}" \
--profile=server "${CERT_SRC_DIR}/mariadb-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/mariadb"
-
-# Validate mariadb certificate and SANs
-openssl x509 -in "${CERT_OUT_DIR}/mariadb.pem" -text -noout
+DOMAINS=(server ldap kafka-ui phpldapadmin postgres mysql mariadb)
+for domain in "${DOMAINS[@]}"
+do
+    # Create ${domain} certificates with the appropriate SANs (SANs listed in ${domain}-domain.json)
+    cfssl gencert -ca="${CA_CERT_PATH}" -ca-key="${CA_KEY_PATH}" -config="${CA_CONFIG_PATH}" \
+      -profile=server "${CERT_SRC_DIR}/${domain}-domain.json" | cfssljson -bare "${CERT_OUT_DIR}/${domain}"
+    # Validate ${domain} certificate and SANs
+    openssl x509 -in "${CERT_OUT_DIR}/${domain}.pem" -text -noout
+done
 
 # Create secret with TLS certificates for the OpenLDAP container
 kubectl create secret generic ldap-sslcerts --save-config --dry-run=client \
@@ -123,17 +68,14 @@ kubectl create secret generic ldap-sslcerts --save-config --dry-run=client \
 kubectl apply -f -
 
 # Deploy OpenLDAP
-helm upgrade --install ldap ${TUTORIAL_HOME}/assets/openldap --namespace confluent
+helm upgrade --install ldap ${TUTORIAL_HOME}/assets/openldap
 kubectl wait --for=condition=Ready pod/ldap-0 --timeout=60s
 
 # Query the OpenLDAP server
-while true; do
-  kubectl --namespace confluent exec -it ldap-0 -- ldapsearch -LLL -x -H ldap://ldap.confluent.svc.cluster.local:389 -b 'dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!'
-  if [ $? -eq 0 ]; then
-    break  # If the command succeeds (exit code 0), exit the loop.
-  else
-    sleep 15  # If the command fails (exit code not 0), wait for 15 seconds and then retry.
-  fi
+SEARCH_CMD=(ldapsearch -LLL -x -H ldap://ldap.confluent.svc.cluster.local:389 -b 'dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!')
+while true
+do
+    kubectl exec -it ldap-0 -- "${SEARCH_CMD[@]}" && break || sleep 5
 done
 
 # Provide component TLS certificates
@@ -160,96 +102,66 @@ kubectl apply -f -
 kubectl create secret generic mds-token --save-config --dry-run=client \
   --from-file=mdsPublicKey.pem="${CERT_SRC_DIR}/mds-publickey.txt" \
   --from-file=mdsTokenKeyPair.pem="${CERT_SRC_DIR}/mds-tokenkeypair.txt" \
-  -o yaml | \
-kubectl apply -f -
-
+  -o yaml | kubectl apply -f -
 
 RBAC_CRED_DIR="${TUTORIAL_HOME}/rbac-credentials"
-# Kafka RBAC credential
-kubectl create secret generic mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/bearer.txt" \
-  -o yaml | \
-kubectl apply -f -
-
-# Control Center RBAC credential
-kubectl create secret generic c3-mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/c3-mds-client.txt" \
-  -o yaml | \
-kubectl apply -f -
-
-# Connect RBAC credential
-kubectl create secret generic connect-mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/connect-mds-client.txt" \
-  -o yaml | \
-kubectl apply -f -
-
-# Schema Registry RBAC credential
-kubectl create secret generic sr-mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/sr-mds-client.txt" \
-  -o yaml | \
-kubectl apply -f -
-
-# ksqlDB RBAC credential
-kubectl create secret generic ksqldb-mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/ksqldb-mds-client.txt" \
-  -o yaml | \
-kubectl apply -f -
-
-# Kafka Rest Proxy RBAC credential
-kubectl create secret generic krp-mds-client --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/krp-mds-client.txt" \
-  -o yaml | \
-kubectl apply -f -
+SERVICES=(mds-client c3-mds-client connect-mds-client sr-mds-client ksqldb-mds-client krp-mds-client)
+for service in "${SERVICES[@]}"
+do
+  kubectl create secret generic "${service}" --save-config --dry-run=client \
+    --from-file=bearer.txt="${RBAC_CRED_DIR}/${service}.txt" \
+    -o yaml | kubectl apply -f -
+done
 
 # Kafka REST credential
 kubectl create secret generic rest-credential --save-config --dry-run=client \
-  --from-file=bearer.txt="${RBAC_CRED_DIR}/bearer.txt" \
-  --from-file=basic.txt="${RBAC_CRED_DIR}/bearer.txt" \
-  -o yaml | \
-kubectl apply -f -
+  --from-file=bearer.txt="${RBAC_CRED_DIR}/mds-client.txt" \
+  --from-file=basic.txt="${RBAC_CRED_DIR}/mds-client.txt" \
+  -o yaml | kubectl apply -f -
 
 # Deploy Confluent Platform
-kubectl apply -f ${TUTORIAL_HOME}/manifests/confluent-platform-production.yaml --namespace confluent
+kubectl apply -f "${TUTORIAL_HOME}/manifests/confluent-platform-production.yaml"
 sleep 15
 PODS=(zookeeper-0 kafka-0 kafka-1 kafka-2 connect-0 schemaregistry-0 ksqldb-0 controlcenter-0)
-for pod in ${PODS[@]}; do
-    kubectl wait --for=condition=Ready --timeout=600s pod/${pod}
+for pod in "${PODS[@]}"
+do
+    kubectl wait --for=condition=Ready --timeout=600s "pod/${pod}"
 done
 
 # Create RBAC Rolebindings for Control Center admin
 ROLE_BIND_DIR="${TUTORIAL_HOME}/rolebindings"
-kubectl --namespace confluent apply -f "${ROLE_BIND_DIR}/controlcenter-testadmin-rolebindings.yaml"
-kubectl --namespace confluent apply -f "${ROLE_BIND_DIR}/controlcenter-connect-rolebindings.yaml"
-kubectl --namespace confluent apply -f "${ROLE_BIND_DIR}/controlcenter-sr-rolebindings.yaml"
+kubectl apply -f "${ROLE_BIND_DIR}/controlcenter-testadmin-rolebindings.yaml"
+kubectl apply -f "${ROLE_BIND_DIR}/controlcenter-connect-rolebindings.yaml"
+kubectl apply -f "${ROLE_BIND_DIR}/controlcenter-sr-rolebindings.yaml"
 
 # Set ACL for user connect
 kubectl exec -it kafka-0 -c kafka -- kafka-acls --bootstrap-server kafka.confluent.svc.cluster.local:9092 --command-config /opt/confluentinc/etc/kafka/kafka.properties --add --allow-principal User:connect --allow-host "*" --operation All --topic "*" --group "*"
 
 # Create secret with keystore and truststore for Kafka-UI container
-openssl pkcs12 -export -in "${CERT_OUT_DIR}/kafka-ui.pem" -inkey "${CERT_OUT_DIR}/kafka-ui-key.pem" -out "${CERT_OUT_DIR}/keystore.p12" -password pass:mystorepassword
-keytool -importcert -storetype PKCS12 -keystore "${CERT_OUT_DIR}/truststore.p12" -storepass mystorepassword -alias ca -file "${CA_CERT_PATH}" -noprompt
+STORE_PASSWORD="mystorepassword"
+openssl pkcs12 -export -in "${CERT_OUT_DIR}/kafka-ui.pem" -inkey "${CERT_OUT_DIR}/kafka-ui-key.pem" -out "${CERT_OUT_DIR}/keystore.p12" -password pass:"${STORE_PASSWORD}"
+keytool -importcert -storetype PKCS12 -keystore "${CERT_OUT_DIR}/truststore.p12" -storepass "${STORE_PASSWORD}" -alias ca -file "${CA_CERT_PATH}" -noprompt
 kubectl create secret generic kafkaui-pkcs12 --save-config --dry-run=client \
   --from-file=cacerts.pem="${CA_CERT_PATH}" \
   --from-file=privkey.pem="${CERT_OUT_DIR}/kafka-ui-key.pem" \
   --from-file=fullchain.pem="${CERT_OUT_DIR}/kafka-ui.pem" \
-  --from-literal=jksPassword.txt=jksPassword=mystorepassword \
+  --from-literal=jksPassword.txt=jksPassword="${STORE_PASSWORD}" \
   --from-file=keystore.p12="${CERT_OUT_DIR}/keystore.p12" \
   --from-file=truststore.p12="${CERT_OUT_DIR}/truststore.p12" \
-  -o yaml | \
-kubectl apply -f -
+  -o yaml | kubectl apply -f -
 
 # Deploy Kafka UI container
 helm upgrade --install kafka-ui kafka-ui/kafka-ui --version 0.7.4 -f "${TUTORIAL_HOME}/manifests/kafkaui-values.yaml"
 POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep kafka-ui)
-kubectl wait --for=condition=Ready pod/${POD_NAME} --timeout=60s
+kubectl wait --for=condition=Ready pod/${POD_NAME} --timeout=600s
 
 # Build custom phpLDAPadmin image
-docker build -t osixia/phpldapadmin-vf:0.9.0 --progress=plain -f "${DOCKER_IMAGE_DIR}/phpldapadmin/Dockerfile" ../
+docker build -t osixia/phpldapadmin-vf:0.9.0 --progress=plain -f "${DOCKER_IMAGE_DIR}/phpldapadmin/Dockerfile" "${TUTORIAL_HOME}"
 
 # Deploy phpLDAPadmin container
 helm upgrade --install phpldapadmin cetic/phpldapadmin --version 0.1.4  -f "${TUTORIAL_HOME}/manifests/phpldapadmin-values.yaml"
 POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep phpldapadmin)
-kubectl wait --for=condition=Ready pod/${POD_NAME} --timeout=60s
+kubectl wait --for=condition=Ready pod/${POD_NAME} --timeout=600s
 kubectl patch service phpldapadmin -p '{"spec": {"ports": [{"name": "https","port": 443,"nodePort": 30902}]}}'
 kubectl patch deployment phpldapadmin -p '{"spec": {"template": {"spec": {"containers": [{"name": "phpldapadmin", "args": ["--copy-service", "--loglevel=debug"]}]}}}}'
 
@@ -263,7 +175,7 @@ kubectl apply -f -
 
 # Deploy PostgreSQL container
 helm upgrade --install postgresql bitnami/postgresql --version 13.0.0 -f "${TUTORIAL_HOME}/manifests/postgres-values.yaml"
-kubectl wait --for=condition=Ready pod/postgresql-0 --timeout=60s
+kubectl wait --for=condition=Ready pod/postgresql-0 --timeout=600s
 
 # Create secret for MySQL container
 kubectl create secret generic mysql-pkcs12 --save-config --dry-run=client \
@@ -275,7 +187,7 @@ kubectl apply -f -
 
 # Deploy MySQL container
 helm upgrade --install mysql bitnami/mysql --version 9.12.3 -f "${TUTORIAL_HOME}/manifests/mysql-values.yaml"
-kubectl wait --for=condition=Ready pod/mysql-0 --timeout=60s
+kubectl wait --for=condition=Ready pod/mysql-0 --timeout=600s
 
 # Create secret for MariaDB container
 kubectl create secret generic mariadb-pkcs12 --save-config --dry-run=client \
@@ -287,9 +199,9 @@ kubectl apply -f -
 
 # Deploy MariaDB container
 helm upgrade --install mariadb bitnami/mariadb --version 13.1.3 -f "${TUTORIAL_HOME}/manifests/mariadb-values.yaml"
-kubectl wait --for=condition=Ready pod/mariadb-0 --timeout=60s
+kubectl wait --for=condition=Ready pod/mariadb-0 --timeout=600s
 
 # Build and deploy Alpine container used for debug
-docker build -t alpine-debug:3.18.3 --progress=plain -f "${DOCKER_IMAGE_DIR}/alpine-debug/Dockerfile" ../
-kubectl apply -f ${TUTORIAL_HOME}/manifests/alpine-debug.yaml
-kubectl wait --for=condition=Ready pod/alpine-debug --timeout=60s
+docker build -t alpine-debug:3.18.3 --progress=plain -f "${DOCKER_IMAGE_DIR}/alpine-debug/Dockerfile" "${TUTORIAL_HOME}"
+kubectl apply -f "${TUTORIAL_HOME}/manifests/alpine-debug.yaml"
+kubectl wait --for=condition=Ready pod/alpine-debug --timeout=600s
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,51 +3,60 @@
 set -xe  # Echo and exit on errors
 
 BIN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 # Set the current tutorial directory
 TUTORIAL_HOME="$(realpath "${BIN_DIR}/..")"
 
 # Set context to confluent
 kubectl config set-context --current --namespace=confluent
 
-for rb in $(kubectl -n confluent get cfrb --no-headers -ojsonpath='{.items[*].metadata.name}'); do kubectl -n confluent  patch cfrb $rb -p '{"metadata":{"finalizers":[]}}' --type=merge; done
+for rb in $(kubectl -n confluent get cfrb --no-headers -ojsonpath='{.items[*].metadata.name}')
+do
+    kubectl -n confluent  patch cfrb $rb -p '{"metadata":{"finalizers":[]}}' --type=merge
+done
 
 kubectl delete confluentrolebinding --all
 kubectl delete --ignore-not-found=true -f "${TUTORIAL_HOME}/manifests/confluent-platform-production.yaml"
 kubectl delete --ignore-not-found=true -f "${TUTORIAL_HOME}/manifests/alpine-debug.yaml"
 
 SECRETS=(
-    ksqldb-mds-client
-    sr-mds-client
-    connect-mds-client
-    krp-mds-client
     c3-mds-client
+    connect-mds-client
+    credential
+    kafkaui-pkcs12
+    krp-mds-client
+    ksqldb-mds-client
+    ldap-sslcerts
+    mariadb-PKCS12
     mds-client 
     mds-token
-    credential
-    ldap-sslcerts
-    tls-group1
-    rest-credential
-    kafkaui-pkcs12
-    postgres-pkcs12
     mysql-pkcs12
-    mariadb-PKCS12
+    postgres-pkcs12
+    rest-credential
+    sr-mds-client
+    tls-group1
 )
-for secret in ${SECRETS[@]}; do
-    kubectl delete --ignore-not-found=true secret ${secret}
+for secret in "${SECRETS[@]}"
+do
+    kubectl delete --ignore-not-found=true secret "${secret}"
 done
 
 RELEASES=(ldap operator kafka-ui phpldapadmin postgresql mysql mariadb)
-for release in ${RELEASES[@]}; do
-    helm status ${release} && helm delete ${release}
+for release in "${RELEASES[@]}"
+do
+    helm status "${release}" && helm delete "${release}"
 done
 
 PVCS=(data-postgresql-0 data-mysql-0 data-mariadb-0 ldap-config-ldap-0 ldap-data-ldap-0)
-for pvc in ${PVCS[@]}; do
-    kubectl delete --ignore-not-found=true pvc ${pvc}
+for pvc in "${PVCS[@]}"
+do
+    kubectl delete --ignore-not-found=true pvc "${pvc}"
 done
 
-kubectl delete --ignore-not-found=true namespace confluent
+kubectl delete --ignore-not-found=true --timeout=120s namespace confluent
+
+# Remove generated certificates
+CERT_OUT_DIR="${TUTORIAL_HOME}/assets/certificates/generated"
+rm -rf "${CERT_OUT_DIR}"
 
 # Uninstall usecases
 "${TUTORIAL_HOME}/usecases/csv/teardown-csv.sh"
